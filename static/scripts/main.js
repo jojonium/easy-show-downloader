3d7218a44f81ec493e1c1a32658d1ccce45bda48
Joseph Petitti <jppetitti@wpi.edu>
2020-01-06 14:57:39 -0500

    add log and download button

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,26 +5,39 @@ let shows;
 
 const getShowsXHR = new XMLHttpRequest();
 const postShowsXHR = new XMLHttpRequest();
+const postDownloadXHR = new XMLHttpRequest();
 
 window.onload = () => {
   getShowsXHR.addEventListener("load", ev => {
     if (getShowsXHR.status === 200) {
+      log("Successfully retrieved server's show list");
       shows = JSON.parse(getShowsXHR.responseText).shows;
       displayShows();
     } else {
-      // TODO handle error
+      log("Error: " + JSON.parse(getShowsXHR.responseText).message, 2);
     }
   });
 
   postShowsXHR.addEventListener("load", ev => {
     if (postShowsXHR.status === 200) {
+      log("Successfully updated server's show list");
       // always update the local show list after sending something to the server
       sendGetShows();
-      // TODO show that it was successfully added
     } else {
-      // TODO handle error
+      log("Error: " + JSON.parse(postShowsXHR.responseText).message, 2);
     }
   });
+
+  postDownloadXHR.addEventListener("load", ev => {
+    // re-enable download button
+    /** @type {HTMLInputElement} */
+    (document.getElementById("post-download")).disabled = false;
+    if (postDownloadXHR.status === 200) {
+      log(JSON.parse(postDownloadXHR.responseText).message);
+    } else {
+      log("Error: " + JSON.parse(postDownloadXHR.responseText).message, 2);
+    }
+  })
 
   sendGetShows();
 };
@@ -65,6 +78,20 @@ const displayShows = () => {
 };
 
 /**
+ * Tells the server to download all new episodes
+ */
+const postDownload = () => {
+  // disable until we get a response so the user doesn't click a bunch of times
+  // in a row
+  /** @type {HTMLInputElement} */
+  (document.getElementById("post-download")).disabled = true;
+   
+  log("Requesting server to download new episodes...");
+  postDownloadXHR.open("POST", "/api/download");
+  postDownloadXHR.send();
+}
+
+/**
  * Removes a show from the list, updating the server
  * @param {number} id 0-based index of show to remove
  */
@@ -79,9 +106,8 @@ const removeShow = id => {
  * @return {false}
  */
 const addShow = formElement => {
-  const input = /** @type {HTMLInputElement} */ (document.getElementById(
-    "new-show"
-  ));
+  const input =
+    /** @type {HTMLInputElement} */ (document.getElementById("new-show"));
   // clean input
   if (input.value === "" || input.value.length >= 128) return false;
   input.value = input.value.trim();
@@ -96,3 +122,19 @@ const addShow = formElement => {
   input.value = "";
   return false;
 };
+
+/**
+ * Displays a string on the document for the user to see
+ * @param {string} str string to write to the log
+ * @param {number} [badness] 0 for normal, 1 for warning, 2 for error
+ */
+const log = (str, badness = 0) => {
+  const line = document.createElement("li");
+  line.innerText = str;
+  if (badness === 1) {
+    line.style.color = "#bfbf94";
+  } else if (badness === 2) {
+    line.style.color = "#c0392b";
+  }
+  document.getElementById("log").appendChild(line);
+}
