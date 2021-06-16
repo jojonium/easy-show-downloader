80bb2b243b187cbb3dc1412b2d2545f716e97d80
Joseph Petitti <joseph@petitti.org>
2021-06-15 22:05:54 -0400

    set up scripts to build back end

---
+++ src/server/app.ts
@@ -0,0 +1,16 @@
+import Hapi from "@hapi/hapi";
+
+const init = async (): Promise<void> => {
+  const server = Hapi.server({
+    port: 8080,
+    host: "localhost",
+  });
+  await server.start();
+  console.log("test");
+  console.log(`Started easy-show-downloader on ${server.info.uri}`);
+};
+
+init().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
