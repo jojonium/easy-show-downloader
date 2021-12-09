c86f7e4461377c915f7f0b7725f0ec340abfd383
Joseph Petitti <joseph@petitti.org>
2021-12-08 20:35:10 -0500

    Fix transmission server connection bug

--- server/src/config.ts
+++ server/src/config.ts
@@ -7,30 +7,30 @@ export const config = {
   DATA_FILE: process.env['DATA_FILE'] ?? 'data.json',
   CRON_SCHEDULE: process.env['CRON_SCHEDULE'] ?? '0 53 * * * *',
   transmission: {
-    HOST: process.env['TRANSMISSION_HOST'] ?? 'localhost',
-    PORT: parseInt(process.env['TRANSMISSION_PORT'] ?? '9091'),
-    USERNAME: process.env['TRANSMISSION_USERNAME'] ?? '',
-    PASSWORD: process.env['TRANSMISSION_PASSWORD'] ?? '',
-    SSL: process.env['TRANSMISSION_PROTOCOL'] === 'https',
-    URL: process.env['TRANSMISSION_URL'] ?? '/transmission/rpc',
+    host: process.env['TRANSMISSION_HOST'] ?? 'localhost',
+    port: parseInt(process.env['TRANSMISSION_PORT'] ?? '9091'),
+    username: process.env['TRANSMISSION_USERNAME'] ?? '',
+    password: process.env['TRANSMISSION_PASSWORD'] ?? '',
+    ssl: process.env['TRANSMISSION_PROTOCOL'] === 'https',
+    url: process.env['TRANSMISSION_URL'] ?? '/transmission/rpc',
   },
 };
 
 export const prettyPrintConfig = () =>
   `{
-  HOST: '${config.HOST}',
-  PORT: ${config.PORT},
+  NODE_HOST: '${config.HOST}',
+  NODE_PORT: ${config.PORT},
   STATIC_DIR: '${config.STATIC_DIR}',
   LOG_STDOUT: ${config.LOG_STDOUT},
   LOG_FILE: ${config.LOG_FILE ? `'${config.LOG_FILE}'` : 'undefined'},
   DATA_FILE: '${config.DATA_FILE}',
   CRON_SCHEDULE: '${config.CRON_SCHEDULE}',
   transmission: {
-    HOST: '${config.transmission.HOST}',
-    PORT: ${config.transmission.PORT},
-    USERNAME: '${config.transmission.USERNAME}',
-    PASSWORD: ${config.transmission.PASSWORD.length > 0 ? '[REDACTED]' : ''},
-    SSL: ${config.transmission.SSL},
-    URL: '${config.transmission.URL}',
+    host: '${config.transmission.host}',
+    port: ${config.transmission.port},
+    username: '${config.transmission.username}',
+    password: ${config.transmission.password.length > 0 ? '[REDACTED]' : ''},
+    ssl: ${config.transmission.ssl},
+    url: '${config.transmission.url}',
   }
 }`;
