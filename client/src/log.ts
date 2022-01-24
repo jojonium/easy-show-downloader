b128b21ccf2a7327275e6291afa549c818947b49
Joseph Petitti <joseph@petitti.org>
2022-01-24 12:58:19 -0500

    Log entries fade after 3s

--- client/src/log.ts
+++ client/src/log.ts
@@ -3,7 +3,13 @@ export const log = (text: string, isError = false): void => {
   if (logContainer === null) throw new Error('Failed to get log container!');
   const line = document.createElement('p');
   line.classList.add('log-line');
-  if (isError) line.classList.add('error');
+  if (isError) {
+    line.classList.add('error');
+  } else {
+    setTimeout(() => line.style.opacity = '0%', 3000);
+    setTimeout(() => line.style.height = '0', 4000);
+    setTimeout(() => logContainer.removeChild(line), 4500);
+  }
   line.innerText = text;
   logContainer.appendChild(line);
 };
