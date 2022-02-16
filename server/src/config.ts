15464e02bdaa7b513517a87bf72eb402a57b8121
Joseph Petitti <joseph@petitti.org>
2022-02-16 12:33:19 -0500

    Refactor server code smells

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,7 +2,7 @@ export const config = {
   HOST: process.env['NODE_HOST'] ?? 'localhost',
   PORT: parseInt(process.env['NODE_PORT'] ?? '3000'),
   STATIC_DIR: process.env['STATIC_DIR'] ?? '../client/public',
-  LOG_STDOUT: !(process.env['LOG_STDOUT'] === 'false'),
+  LOG_STDOUT: process.env['LOG_STDOUT'] !== 'false',
   LOG_FILE: process.env['LOG_FILE'] ?? undefined,
   DATA_FILE: process.env['DATA_FILE'] ?? 'data.json',
   CRON_SCHEDULE: process.env['CRON_SCHEDULE'] ?? '0 53 * * * *',
@@ -22,7 +22,7 @@ export const prettyPrintConfig = () =>
   NODE_PORT: ${config.PORT},
   STATIC_DIR: '${config.STATIC_DIR}',
   LOG_STDOUT: ${config.LOG_STDOUT},
-  LOG_FILE: ${config.LOG_FILE ? `'${config.LOG_FILE}'` : 'undefined'},
+  LOG_FILE: ${config.LOG_FILE ? '\'' + config.LOG_FILE + '\'' : 'undefined'},
   DATA_FILE: '${config.DATA_FILE}',
   CRON_SCHEDULE: '${config.CRON_SCHEDULE}',
   transmission: {
