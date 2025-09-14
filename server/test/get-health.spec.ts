6c78f1f0521d75d66c155974484e1092859b14c1
Joseph Petitti <joseph@petitti.org>
2025-09-14 16:40:40 -0400

    Fix Chai tests and add ts-node as explicit dependency

--- server/test/get-health.spec.ts
+++ server/test/get-health.spec.ts
@@ -1,7 +1,7 @@
 import {app as server, shutDown} from '../src/index';
 import fs from 'fs';
 import chai, {expect} from 'chai';
-import chaiHttp from 'chai-http';
+import {default as chaiHttp, request} from 'chai-http';
 import {config} from '../src/config';
 
 chai.use(chaiHttp);
@@ -23,13 +23,13 @@ describe('GET /api/health', () => {
   });
 
   it('Should normally return 200', async () => {
-    const res = await chai.request(server).get('/api/health');
+    const res = await request.execute(server).get('/api/health');
     expect(res).to.have.status(200);
   });
 
   it('Should return a 500 if data file is unreadable', async () => {
     await fs.promises.writeFile(fileName, 'ai0bj3n1');
-    const res = await chai.request(server).get('/api/health');
+    const res = await request.execute(server).get('/api/health');
     expect(res).to.have.status(500);
     await fs.promises.rm(fileName, {force: true});
   });
