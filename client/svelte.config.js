23ee71fb25fd39a81d1c1a360967028ec2b649ad
Joseph Petitti <joseph@petitti.org>
2026-02-04 22:08:45 -0500

    Update packages (#48)
    * Update packages
    
    * Update more packages

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -15,9 +15,6 @@ const config = {
   kit: {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    files: {
-      appTemplate: "src/App.svelte"
-    },
     version: {
       name: pkg.version
     }
